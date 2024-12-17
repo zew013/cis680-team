eb317e472845460fc19ab8ef72fc2f8bc90e80d7
Keqi Wu <keqiwu@seas.upenn.edu>
2024-12-17 18:19:43 -0500

    Add Training and Finetuning

---
+++ finetune_v2.py
@@ -0,0 +1,354 @@
+import os
+import argparse
+import sys
+from collections import defaultdict, deque
+import pickle
+
+import numpy as np
+from PIL import Image
+import cv2
+
+import torch
+torch.multiprocessing.set_sharing_strategy('file_system')
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import torch.distributed as dist
+from torchvision import transforms
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+import segmentation_models_pytorch as smp
+
+from transformers.models.maskformer.modeling_maskformer import dice_loss, sigmoid_focal_loss
+
+# Add the SAM directory to the system path
+# sys.path.append("./segment-anything")
+sys.path.append("./Full_Segment_Anything")
+# from segment_anything import sam_model_registry
+from Full_Segment_Anything.build_sam import sam_model_registry
+
+NUM_WORKERS = 0  # https://github.com/pytorch/pytorch/issues/42518
+NUM_GPUS = torch.cuda.device_count()
+DEVICE = 'cuda'
+print(NUM_WORKERS, NUM_GPUS, DEVICE)
+def collate_fn(batch):
+    images, bboxes, masks = zip(*batch)
+    images = torch.stack(images, dim=0)
+    return images, bboxes, masks
+
+
+class SAMFinetuner(pl.LightningModule):
+
+    def __init__(
+            self,
+            model_type,
+            checkpoint_path,
+            freeze_image_encoder=False,
+            freeze_prompt_encoder=False,
+            freeze_mask_decoder=False,
+            batch_size=4,
+            learning_rate=1e-4,
+            weight_decay=1e-4,
+            train_dataset=None,
+            val_dataset=None,
+            metrics_interval=10,
+            # original_img_size = 1024,
+            patch_size = 16,
+            # new_img_size = (256, 256),
+        ):
+        super(SAMFinetuner, self).__init__()
+
+        self.model_type = model_type
+        self.model = sam_model_registry[self.model_type](checkpoint=checkpoint_path, custom_img_size = 256, vit_patch_size=patch_size)
+        self.model.to(device=self.device)
+        self.model.train()
+
+        
+        self.freeze_image_encoder = freeze_image_encoder
+        if freeze_image_encoder:
+            for param in self.model.image_encoder.parameters():
+               param.requires_grad = False
+        if freeze_prompt_encoder:
+            for param in self.model.prompt_encoder.parameters():
+               param.requires_grad = False
+        if freeze_mask_decoder:
+            for param in self.model.mask_decoder.parameters():
+                param.requires_grad = False
+        
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.weight_decay = weight_decay
+
+        self.train_dataset = train_dataset
+        self.val_dataset = val_dataset
+
+        self.train_metric = defaultdict(lambda: deque(maxlen=metrics_interval))
+        self.val_metric_per_step = defaultdict(lambda: deque())
+        self.val_metric = []
+        self.train_losses = []
+        self.val_losses = []
+        self.val_step_loss = []
+        self.metrics_interval = metrics_interval
+
+    def forward(self, imgs, bboxes, labels):
+        _, _, H, W = imgs.shape
+        features = self.model.image_encoder(imgs)
+        num_masks = sum([len(b) for b in bboxes])
+
+        loss_focal = loss_dice = loss_iou = 0.
+        predictions = []
+        tp, fp, fn, tn = [], [], [], []
+        for feature, bbox, label in zip(features, bboxes, labels):
+            # Embed prompts
+            sparse_embeddings, dense_embeddings = self.model.prompt_encoder(
+                points=None,
+                boxes=bbox,
+                masks=None,
+            )
+            # Predict masks
+            low_res_masks, iou_predictions = self.model.mask_decoder(
+                image_embeddings=feature.unsqueeze(0),
+                image_pe=self.model.prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=False,
+            )
+            # Upscale the masks to the original image resolution
+            masks = F.interpolate(
+                low_res_masks,
+                (H, W),
+                mode="bilinear",
+                align_corners=False,
+            )
+            predictions.append(masks)
+            # Compute the iou between the predicted masks and the ground truth masks
+            batch_tp, batch_fp, batch_fn, batch_tn = smp.metrics.get_stats(
+                masks,
+                label.unsqueeze(1),
+                mode='binary',
+                threshold=0.5,
+            )
+            batch_iou = smp.metrics.iou_score(batch_tp, batch_fp, batch_fn, batch_tn)
+            # Compute the loss
+            masks = masks.squeeze(1).flatten(1)
+            label = label.flatten(1)
+            loss_focal += sigmoid_focal_loss(masks, label.float(), num_masks)
+            loss_dice += dice_loss(masks, label.float(), num_masks)
+            loss_iou += F.mse_loss(iou_predictions, batch_iou, reduction='sum') / num_masks
+            tp.append(batch_tp)
+            fp.append(batch_fp)
+            fn.append(batch_fn)
+            tn.append(batch_tn)
+        return {
+            'loss': 20. * loss_focal + loss_dice + loss_iou,  # SAM default loss
+            'loss_focal': loss_focal,
+            'loss_dice': loss_dice,
+            'loss_iou': loss_iou,
+            'predictions': predictions,
+            'tp': torch.cat(tp),
+            'fp': torch.cat(fp),
+            'fn': torch.cat(fn),
+            'tn': torch.cat(tn),
+        }
+    
+    def training_step(self, batch, batch_nb):
+        imgs, bboxes, labels = batch
+        outputs = self(imgs, bboxes, labels)
+
+        for metric in ['tp', 'fp', 'fn', 'tn']:
+            self.train_metric[metric].append(outputs[metric])
+
+        # aggregate step metics
+        step_metrics = [torch.cat(list(self.train_metric[metric])) for metric in ['tp', 'fp', 'fn', 'tn']]
+        per_mask_iou = smp.metrics.iou_score(*step_metrics, reduction="micro-imagewise")
+        metrics = {
+            # "loss": outputs["loss"],
+            # "loss_focal": outputs["loss_focal"],
+            # "loss_dice": outputs["loss_dice"],
+            # "loss_iou": outputs["loss_iou"],
+            # "train_per_mask_iou": per_mask_iou,
+            "loss": outputs["loss"],
+            "iou": per_mask_iou,
+            "l_f": outputs["loss_focal"],
+            "l_d": outputs["loss_dice"],
+            "l_i": outputs["loss_iou"],
+        }
+        self.train_losses.append(metrics["loss"].item())
+        self.log_dict(metrics, prog_bar=True, rank_zero_only=True, sync_dist=True)
+        return metrics
+    
+    def validation_step(self, batch, batch_nb):
+        imgs, bboxes, labels = batch
+        outputs = self(imgs, bboxes, labels)
+        for metric in ['tp', 'fp', 'fn', 'tn']:
+            self.val_metric_per_step[metric].append(outputs[metric])
+
+        # aggregate step metics
+        step_metrics = [torch.cat(list(self.val_metric_per_step[metric])) for metric in ['tp', 'fp', 'fn', 'tn']]
+        per_mask_iou = smp.metrics.iou_score(*step_metrics, reduction="micro-imagewise")
+        metrics = {
+            # "loss": outputs["loss"],
+            # "loss_focal": outputs["loss_focal"],
+            # "loss_dice": outputs["loss_dice"],
+            # "loss_iou": outputs["loss_iou"],
+            # "train_per_mask_iou": per_mask_iou,
+            "val_loss": outputs["loss"],
+            "viou": per_mask_iou,
+            "vl_f": outputs["loss_focal"],
+            "vl_d": outputs["loss_dice"],
+            "vl_i": outputs["loss_iou"],
+        }
+
+        self.val_step_loss.append(metrics["val_loss"].item())
+        self.log_dict(metrics, prog_bar=True, rank_zero_only=True, sync_dist=True)
+        
+        outputs.pop("predictions")
+        self.val_metric.append(outputs)
+        return outputs
+        
+    def on_train_end(self):
+        results = {
+            "train_losses": self.train_losses,
+            "val_losses": self.val_losses,
+        }
+        with open("training_metrics.pkl", "wb") as f:
+            pickle.dump(results, f)
+        print("Saved training and validation losses to training_metrics.pkl.")
+        
+    def on_validation_epoch_start(self):
+        self.val_metric = []
+    
+    def on_validation_epoch_end(self):
+        # if NUM_GPUS > 1:
+        #     outputs = all_gather(outputs)
+        #     # the outputs are a list of lists, so flatten it
+        #     outputs = [item for sublist in outputs for item in sublist]
+        # aggregate step metics
+        step_metrics = [
+            torch.cat(list([x[metric].to(self.device) for x in self.val_metric]))
+            for metric in ['tp', 'fp', 'fn', 'tn']]
+        # per mask IoU means that we first calculate IoU score for each mask
+        # and then compute mean over these scores
+        per_mask_iou = smp.metrics.iou_score(*step_metrics, reduction="micro-imagewise")
+        average_val_loss = sum(self.val_step_loss) / len(self.val_step_loss)
+        self.val_losses.append(average_val_loss)
+        metrics = {"val_per_mask_iou": per_mask_iou, "val_loss": average_val_loss}
+        self.log_dict(metrics, sync_dist=True)
+        self.val_step_loss = []
+        return metrics
+    
+    def configure_optimizers(self):
+        opt = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        def warmup_step_lr_builder(warmup_steps, milestones, gamma):
+            def warmup_step_lr(steps):
+                if steps < warmup_steps:
+                    lr_scale = (steps + 1.) / float(warmup_steps)
+                else:
+                    lr_scale = 1.
+                    for milestone in sorted(milestones):
+                        if steps >= milestone * self.trainer.estimated_stepping_batches:
+                            lr_scale *= gamma
+                return lr_scale
+            return warmup_step_lr
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+            opt,
+            warmup_step_lr_builder(250, [0.66667, 0.86666], 0.1)
+        )
+        return {
+            'optimizer': opt,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                'interval': "step",
+                'frequency': 1,
+            }
+        }
+    
+    def train_dataloader(self):
+        train_loader = torch.utils.data.DataLoader(
+            self.train_dataset,
+            collate_fn=collate_fn,
+            batch_size=self.batch_size,
+            num_workers=NUM_WORKERS,
+            shuffle=True)
+        return train_loader
+    
+    def val_dataloader(self):
+        val_loader = torch.utils.data.DataLoader(
+            self.val_dataset,
+            collate_fn=collate_fn,
+            batch_size=self.batch_size,
+            num_workers=NUM_WORKERS,
+            shuffle=False)
+        return val_loader
+
+
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--data_root", type=str, required=True, help="path to the data root")
+#     parser.add_argument("--model_type", type=str, required=True, help="model type", choices=['vit_h', 'vit_l', 'vit_b'])
+#     parser.add_argument("--checkpoint_path", type=str, required=True, help="path to the checkpoint")
+#     parser.add_argument("--freeze_image_encoder", action="store_true", help="freeze image encoder")
+#     parser.add_argument("--freeze_prompt_encoder", action="store_true", help="freeze prompt encoder")
+#     parser.add_argument("--freeze_mask_decoder", action="store_true", help="freeze mask decoder")
+#     parser.add_argument("--batch_size", type=int, default=1, help="batch size")
+#     parser.add_argument("--image_size", type=int, default=1024, help="image size")
+#     parser.add_argument("--steps", type=int, default=1500, help="number of steps")
+#     parser.add_argument("--learning_rate", type=float, default=1e-4, help="learning rate")
+#     parser.add_argument("--weight_decay", type=float, default=1e-2, help="weight decay")
+#     parser.add_argument("--metrics_interval", type=int, default=50, help="interval for logging metrics")
+#     parser.add_argument("--output_dir", type=str, default=".", help="path to save the model")
+
+#     args = parser.parse_args()
+
+#     # load the dataset
+#     train_dataset = Coco2MaskDataset(data_root=args.data_root, split="train", image_size=args.image_size)
+#     val_dataset = Coco2MaskDataset(data_root=args.data_root, split="val", image_size=args.image_size)
+
+#     # create the model
+#     model = SAMFinetuner(
+#         args.model_type,
+#         args.checkpoint_path,
+#         freeze_image_encoder=args.freeze_image_encoder,
+#         freeze_prompt_encoder=args.freeze_prompt_encoder,
+#         freeze_mask_decoder=args.freeze_mask_decoder,
+#         train_dataset=train_dataset,
+#         val_dataset=val_dataset,
+#         batch_size=args.batch_size,
+#         learning_rate=args.learning_rate,
+#         weight_decay=args.weight_decay,
+#         metrics_interval=args.metrics_interval,
+#     )
+
+#     callbacks = [
+#         LearningRateMonitor(logging_interval='step'),
+#         ModelCheckpoint(
+#             dirpath=args.output_dir,
+#             filename='{step}-{val_per_mask_iou:.2f}',
+#             save_last=True,
+#             save_top_k=1,
+#             monitor="val_per_mask_iou",
+#             mode="max",
+#             save_weights_only=True,
+#             every_n_train_steps=args.metrics_interval,
+#         ),
+#     ]
+#     trainer = pl.Trainer(
+#         strategy='ddp' if NUM_GPUS > 1 else None,
+#         accelerator=DEVICE,
+#         devices=NUM_GPUS,
+#         precision=32,
+#         callbacks=callbacks,
+#         max_epochs=-1,
+#         max_steps=args.steps,
+#         val_check_interval=args.metrics_interval,
+#         check_val_every_n_epoch=None,
+#         num_sanity_val_steps=0,
+#     )
+
+#     trainer.fit(model)
+
+
+# if __name__ == "__main__":
+#     main()
---
+++ train.py
@@ -0,0 +1,269 @@
+import glob
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from pycocotools import mask as mask_utils
+
+import json
+import numpy as np
+from tqdm import tqdm
+
+# new_height, new_width  = 160, 256
+new_height, new_width  = 256, 256
+input_transforms = transforms.Compose([
+    transforms.Resize((new_height, new_width), antialias=True),
+    transforms.ToTensor(),
+])
+
+target_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((new_height, new_width), antialias=True),
+])
+
+def resize_bbox(bbox, original_width, original_height, new_width=256, new_height=160):
+    """
+    Resize bounding box coordinates based on the new image dimensions.
+
+    Args:
+        bbox (list or tuple): The original bounding box [x, y, width, height].
+        original_width (int or float): The original width of the image.
+        original_height (int or float): The original height of the image.
+        new_width (int or float): The new width to resize the image to.
+        new_height (int or float): The new height to resize the image to.
+
+    Returns:
+        list: The resized bounding box [new_x, new_y, new_width, new_height].
+    """
+    # Calculate scaling factors for width and height
+    scale_x = new_width / original_width
+    scale_y = new_height / original_height
+
+    # Unpack the original bounding box
+    x, y, w, h = bbox
+
+    # Apply scaling factors to each component of the bounding box
+    new_x = x * scale_x
+    new_y = y * scale_y
+    new_w = w * scale_x
+    new_h = h * scale_y
+
+    return [new_x, new_y, new_x + new_w, new_y + new_h]
+
+
+
+class SA1B_Dataset(torchvision.datasets.ImageFolder):
+    """A data loader for the SA-1B Dataset from "Segment Anything" (SAM)
+    This class inherits from :class:`~torchvision.datasets.ImageFolder` so
+    the same methods can be overridden to customize the dataset.
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of an Image file
+            and check if the file is a valid file (used to check of corrupt files)
+     Attributes:
+        classes (list): List of the class names sorted alphabetically.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, _ = self.imgs[index] # discard automatic subfolder labels
+        image = self.loader(path)
+        masks = json.load(open(f'{path[:-3]}json'))['annotations'] # load json masks
+        # print(json.load(open(f'{path[:-3]}json')))
+        seg = []
+        bbox = []
+
+        for m in masks:
+            # decode masks from COCO RLE format
+            seg_piece = mask_utils.decode(m['segmentation'])
+            original_height, original_width = seg_piece.shape
+            seg.append(seg_piece)
+            bbox.append(resize_bbox(m['bbox'], original_width, original_height, new_width=new_width, new_height=new_height))
+            
+        seg = np.stack(seg, axis=-1)
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            seg = self.target_transform(seg)
+        seg[seg > 0] = 1 # convert to binary masks
+
+        return image, torch.as_tensor(bbox), torch.as_tensor(seg).long()
+        
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+input_reverse_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+])
+
+import matplotlib.pyplot as plt
+def show_image(image, bbox, masks, num_rows=12, num_cols=12):
+    # image: numpy image
+    # target: mask [N, H, W]
+    # fig, axs = plt.subplots(row, col, figsize=(20, 12))
+    # for i in range(row):
+    #     for j in range(col):
+    #         if i*row+j < target.shape[0]:
+    #             axs[i, j].imshow(image)
+    #             axs[i, j].imshow(target[i*row+j], alpha=0.5)
+    #         else:
+    #             axs[i, j].imshow(image)
+    #         axs[i, j].axis('off')
+
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(20, 12))
+    total = num_rows * num_cols
+    
+    for i in range(num_rows):
+        for j in range(num_cols):
+            idx = i * num_cols + j
+            ax = axs[i, j]
+            if idx < masks.shape[0]:
+                ax.imshow(image)
+                ax.imshow(masks[idx], alpha=0.5, cmap='jet')  # Use a colormap for better visibility
+                # Draw bounding box
+                box = bbox[idx].numpy()
+                rect = plt.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1],
+                                     linewidth=1, edgecolor='r', facecolor='none')
+                ax.add_patch(rect)
+            else:
+                ax.imshow(image)
+            ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+
+from finetune import *
+import random
+from torch.utils.data import Subset
+
+path = './sa1b'
+dataset = SA1B_Dataset(path, transform=input_transforms, target_transform=target_transforms)
+image, bbox, masks = dataset.__getitem__(3535)
+
+# Set a manual seed for reproducibility
+torch.manual_seed(42)
+
+subset_ratio = 0.3
+subset_size = int(subset_ratio * len(dataset))
+torch.manual_seed(42)
+subset_indices = random.sample(range(len(dataset)), subset_size)
+
+subset_dataset = Subset(dataset, subset_indices)
+
+from torch.utils.data import random_split
+train_ratio = 0.8
+val_ratio = 0.2
+
+# Calculate the number of samples for each set
+train_size = int(train_ratio * len(subset_dataset))
+val_size = len(subset_dataset) - train_size
+
+print(f"Total samples: {len(subset_dataset)}")
+print(f"Training samples: {train_size}")
+print(f"Validation samples: {val_size}")
+
+
+# Split the dataset
+train_dataset, val_dataset = random_split(subset_dataset, [train_size, val_size])
+
+print(f"Training dataset length: {len(train_dataset)}")
+print(f"Validation dataset length: {len(val_dataset)}")
+
+NUM_WORKERS = 15  # https://github.com/pytorch/pytorch/issues/42518
+NUM_GPUS = torch.cuda.device_count()
+DEVICE = 'cuda'
+print(NUM_WORKERS, NUM_GPUS, DEVICE)
+
+from types import SimpleNamespace
+from pytorch_lightning.strategies import DDPStrategy
+
+def dict_to_namespace(d):
+    return SimpleNamespace(**d)
+
+config_dict = {
+    "model_type": "vit_b",
+    "checkpoint_path": "sam_vit_b_01ec64.pth",
+    "freeze_image_encoder": False,
+    "freeze_prompt_encoder": False,
+    "freeze_mask_decoder": False,
+    "batch_size": 4,
+    'original_img_size': 1024,
+    'patch_size': 16,
+    'new_img_size': (256, 256),
+    "steps": 168,
+    "learning_rate": 1e-4,
+    "weight_decay": 1e-2,
+    "metrics_interval": 20,
+    "output_dir": 'project_checkpoints/'
+}
+
+args = dict_to_namespace(config_dict)
+
+model = SAMFinetuner(
+    args.model_type,
+    args.checkpoint_path,
+    freeze_image_encoder=args.freeze_image_encoder,
+    freeze_prompt_encoder=args.freeze_prompt_encoder,
+    freeze_mask_decoder=args.freeze_mask_decoder,
+    train_dataset=train_dataset,
+    val_dataset=val_dataset,
+    batch_size=args.batch_size,
+    learning_rate=args.learning_rate,
+    weight_decay=args.weight_decay,
+    metrics_interval=args.metrics_interval,
+    original_img_size = 1024,
+    patch_size = 16,
+    new_img_size = (256, 256)
+)
+
+callbacks = [
+    LearningRateMonitor(logging_interval='epoch'),
+    ModelCheckpoint(
+        dirpath=args.output_dir,
+        filename='{step}-{val_per_mask_iou:.2f}',
+        save_last=True,
+        save_top_k=10,
+        monitor="val_per_mask_iou",
+        mode="max",
+        save_weights_only=True,
+        every_n_train_steps=args.metrics_interval,
+    ),
+]
+from pytorch_lightning.loggers import TensorBoardLogger
+logger = TensorBoardLogger(save_dir='lightning_logs/', name='sam_finetuning')
+
+torch.set_float32_matmul_precision('high')
+
+trainer = pl.Trainer(
+    strategy='ddp_find_unused_parameters_true' if NUM_GPUS > 1 else 'auto',
+    accelerator=DEVICE,
+    devices=NUM_GPUS,
+    precision=32,
+    callbacks=callbacks,
+    max_epochs=-1,
+    max_steps=args.steps,
+    val_check_interval=args.metrics_interval,
+    check_val_every_n_epoch=None,
+    num_sanity_val_steps=2,
+    logger=logger
+)
+
+trainer.fit(model)
+
